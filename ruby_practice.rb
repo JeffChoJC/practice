c9d82f803b5d8569901031d1a285e9f20c07eedf
Jeffrey <chojeffrey@gmail.com>
2019-10-11 17:30:18 -0400

    Update

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -1,2 +1,5 @@
 # what if we started by practicing a little bit of ruby to refresh 
 # my memory but mostly practice in javascript daily
+
+# haven't gotten started yet but i figure its cause its friday
+# we have to try again tmrw even if its saturday
