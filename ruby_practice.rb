1105c064becb6f12169ebad70f8c9025bac194bf
Jeffrey <chojeffrey@gmail.com>
2019-10-13 16:15:47 -0400

    Update

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -3,3 +3,6 @@
 
 # haven't gotten started yet but i figure its cause its friday
 # we have to try again tmrw even if its saturday
+
+# saturday failed and now its sunday
+# i definitely need to do something about maru because its affecting my mental and productivity
