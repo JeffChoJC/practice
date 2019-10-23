18a086e69366dd8a76529ae43c12a22d932be56c
Jeffrey <chojeffrey@gmail.com>
2019-10-23 00:26:49 -0400

    Practice

--- example_tech.js
+++ example_tech.js
@@ -5,23 +5,41 @@ class LeaderBoard {
 
     addScore(playerId, score) {  
         if (this.board[playerId]) {
-            this.board[playerId].scores.push(score);
+            this.board[playerId].addScore(score);
         } else {
             this.board[playerId] = new Player(playerId, score);
         }
 
-        const scores = this.board[playerId].scores;
-        return this.board[playerId].average = scores.reduce((sum, num) => {
-            return sum + num
-        }) / scores.length;
+        return this.board[playerId].average();
     }
 
     top(num) {
-        let averages = [];
+        let players = [];
         for (var key in this.board) {
-            averages.push([key, this.board[key].average]);
-            console.log(averages);
+            players.push(this.board[key])
         }
+
+        let sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (var i = 0; i < players.length - 1; i++) {
+                if (players[i].average() < players[i + 1].average()) {
+                    [players[i], players[i + 1]] = [players[i + 1], players[i]];
+                    sorted = false;
+                }
+            }
+        }
+
+        let top = [];
+        for (var j = 0; j < num; j++) {
+            top.push(players[j].id);
+        }        
+
+        return top;
+    }
+
+    reset(playerId) {
+        this.board[playerId].reset();
     }
 }
 
@@ -29,12 +47,37 @@ class Player {
     constructor(id, score) {
         this.id = id;
         this.scores = [score];
-        this.average = score;
+    }
+
+    addScore(score) {
+        this.scores.push(score);
+    }
+
+    average() {
+        return this.scores.reduce((sum, num) => {
+            return sum + num
+        }) / this.scores.length;
+    }
+
+    reset() {
+        this.scores = [];
     }
 }
 
 test = new LeaderBoard();
 test.addScore(1, 80);
 test.addScore(1, 60);
+test.addScore(2, 60);
+test.addScore(2, 65);
+test.addScore(3, 80);
+test.addScore(4, 70);
+test.addScore(4, 90);
+test.addScore(4, 45);
+test.addScore(5, 50);
+test.addScore(6, 100);
+console.log(test.board);
+test.reset(6);
+console.log("-------------");
+console.log(test.board);
 // console.log(test.board);
-console.log(test.top(3));
+// console.log(test.top(5));
--- js_practice.js
+++ js_practice.js
@@ -15,4 +15,20 @@ function quicksort(nums) {
     return quicksort(less).concat(pivot).concat(quicksort(more));
 }
 
-console.log(quicksort([12, 31, 51, 2, 86, 142, 754, 22, 61, 23, 9, 0, 11]));
+function bubblesort(nums) {
+    let sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (var i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+                sorted = false;
+            }
+        }
+    }
+    return nums;
+}
+
+var nums1 = [12, 31, 51, 2, 86, 142, 754, 22, 61, 23, 9, 0, 11];
+// console.log(quicksort(nums1));
+console.log(bubblesort(nums1));
